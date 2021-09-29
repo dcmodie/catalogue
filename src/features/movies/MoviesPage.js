eda4aae9bf8759d8d380945f68d5348fdb653c90
Doug  Modie <dmodie@gmail.com>
2021-09-28 19:41:39 -0700

    sjajaj

--- src/features/movies/MoviesPage.js
+++ src/features/movies/MoviesPage.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import MovieListItem from './MovieListItem'
 import {useSelector, useDispatch } from 'react-redux';
+import Modal from "react-modal";
+import './movies.css'
 
 const Movies = (props)=>{
 	const dispatch = useDispatch();
 	const movies = useSelector((state)=>state.movies);
-	const [inVal, setInVal] = useState('aaaa');
+	const [inVal, setInVal] = useState('');
+	const [addMovieModalOpen, setAddMovieModalOpen] = useState(false);
 
 	const renderList = (movies) => {
 		const retList = []
@@ -22,9 +25,50 @@ const Movies = (props)=>{
 
 	return (
 		<div>
-			<div><input value={inVal} onChange={onInputChange}/></div>
+			<div><input value={inVal} onChange={onInputChange} /></div>
+			<button onClick={(e)=>{setAddMovieModalOpen(true)}}>Add Movie</button>
+			<Modal
+				isOpen={addMovieModalOpen}
+				onRequestClose={(e)=>{setAddMovieModalOpen(false)}}
+				contentLabel="Add Movies"
+				className="addMoviesModal">
+				<div>my title</div>
+				<label>Title<input value={inVal} onChange={onInputChange} /></label>
+				<button onClick={(e)=>{ setAddMovieModalOpen(false) }}>Close modal</button>
+			</Modal>
 			{renderList(movies)}
 
 		</div>)
 }
 export default Movies
+
+
+// import React, { useState } from "react";
+// import "./styles.css";
+
+// import Modal from "react-modal";
+
+// //Modal.setAppElement("#root");
+
+// export default function App() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   function toggleModal() {
+//     setIsOpen(!isOpen);
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick={toggleModal}>Open modal</button>
+
+//       <Modal
+//         isOpen={isOpen}
+//         onRequestClose={toggleModal}
+//         contentLabel="My dialog"
+//       >
+//         <div>My modal dialog.</div>
+//         <button onClick={toggleModal}>Close modal</button>
+//       </Modal>
+//     </div>
+//   );
+// }
